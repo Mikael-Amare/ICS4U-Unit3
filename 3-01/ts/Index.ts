8710083f39aabfb6788afc740d72277ac7d9687c
Mikael Amare <mikael.amare@mths.ca>
2024-11-18 22:54:16 +0000

    added a unit 3 template file

--- 3-01/ts/Index.ts
+++ 3-01/ts/Index.ts
@@ -6,3 +6,28 @@
  * @Since Nov. 18 2024
  */
 
+/**
+ * 
+ * @param accepts string
+ * @return the string backwards. using recursion 
+ */
+
+function reverseString(stringToReverse: String): String {
+  
+  if (stringToReverse == ``) {
+    return ``
+  } else {
+    firstCharacter = stringToReverse[0]
+    restOfString = stringToReverse[1]
+    return reversedString(RestOfString) + firstCharacter
+   }
+}
+
+function main(): Void {
+    // calls a recursive function
+
+    console.log(``)
+    console.log(`The original string is: ${stringToReverse}`)
+    theReversedString = reverseString(stringToReverse)
+    console.log(`The reversed string is: ${theReversedString}`)
+}
