68718a87aebb673b62da53f607b33d5c86ecd3c6
Mikael Amare <mikael.amare@mths.ca>
2024-11-19 14:37:27 +0000

    doing linting errors

--- 3-01/C++/Main.cpp
+++ 3-01/C++/Main.cpp
@@ -4,6 +4,7 @@
  * @Author Mikael Amare
  * @Version 1.0
  * @Since Nov. 18 2024
+ * Copyright [2024] Mikael Amare
  */
 
 #include <iostream>
@@ -14,7 +15,8 @@ static auto reverseString(const std::string& stringToReverse) -> std::string {
         return "";
     } else {
         const char firstCharacter = stringToReverse[0];
-        const std::string restOfString = stringToReverse.substr(1); // Get the rest of the string
+        const std::string restOfString = stringToReverse.substr(1);
+        // Get the rest of the string
         return reverseString(restOfString) + firstCharacter;
     }
 }
@@ -22,12 +24,11 @@ static auto reverseString(const std::string& stringToReverse) -> std::string {
 int main() {
     std::string stringToReverse;
     std::cout << "Enter a string: ";
-    std::getline(std::cin, stringToReverse); // Use getline to handle spaces in input
+    std::getline(std::cin, stringToReverse); 
+    // Use getline to handle spaces in input
 
     std::cout << "The original string is: " << stringToReverse << '\n';
-
     const std::string theReversedString = reverseString(stringToReverse);
-
     std::cout << "The reversed string is: " << theReversedString << '\n';
 
     return 0;
