88cf9be0eb9372c27b565d982deac1f8c4a1b0d2
Mikael Amare <mikael.amare@mths.ca>
2024-11-22 01:10:33 +0000

    implanted test code into index

--- 3-02/ts/index.ts
+++ 3-02/ts/index.ts
@@ -1,13 +1,20 @@
-/* Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program calculates factorial
-*/
+/*
+ * This program uses recursion to
+ * calculate factorial of a number
+ * @Author Mikael Amare
+ * @Version 1.0
+ * @Since Nov. 21 2024
+ */
+
+/**
+ * Reverses a string using recursion.
+ * @param userInput [number] The string to do factorial.
+ * @return The userInput string.
+ */
 
 function factorial(userInputInt: number) {
   // recursive function calculating factorial
-  if (isNaN(userInputInt)) {
-    return -1
-  } else if (userInputInt === 1) {
+  if (userInputInt === 1) {
     return 1;
   } else if (userInputInt === 0) {
     return 1
@@ -16,20 +23,21 @@ function factorial(userInputInt: number) {
   }
 }
 
-// input
-const userInput = (prompt(`Enter a positive integer: `));
-if (userInput == null) {
-  console.log(`No input was made.`)
-} else if (userInput < 0) {
-  console.log(`That was not a positive integer.`);
+const userInput = prompt(`Enter a positive integer: `);
+
+if (userInput === null) {
+  console.log(`That was not a number.`);
 } else {
-  const userInputInt = parseInt(userInput)
-  // call function
-  const factorialAnswer = factorial(userInputInt);
-  if (factorialAnswer == -1) {
-    console.log(`That was not a number.`)
+  const userInputInt = parseInt(userInput);
+
+  if (isNaN(userInputInt)) {
+    console.log(`That was not a number.`);
+  } else if (userInputInt < 0) {
+    console.log(`That was not a positive integer.`);
   } else {
-    console.log(`The factorial of ${userInput} is ${factorialAnswer}`);
+    // Call function
+    const factorialAnswer = factorial(userInputInt);
+    console.log(`The factorial of ${userInputInt} is ${factorialAnswer}`);
   }
 }
-console.log(`\nDone.`);
+console.log(`\nDone`)
