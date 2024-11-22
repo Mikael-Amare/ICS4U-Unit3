0e0bef6823df03e293067a32a84e56e785063c83
Mikael Amare <mikael.amare@mths.ca>
2024-11-22 03:11:27 +0000

    fixed spacing and tiny errors

--- 3-02/Cpp/Main.cpp
+++ 3-02/Cpp/Main.cpp
@@ -12,41 +12,41 @@
 #include <sstream>
 
 // Function to calculate factorial recursively
-int factorial(int number) {
-   if (userInputInt < 0) {
-      return -1
-   } else if (number == 0 || number == 1) {
-       return 1;
-   } else {
-       return number * factorial(number - 1);
-   }
+int factorial(int userInputInt) {
+    if (userInputInt < 0) {
+        return -1;
+    } else if (userInputInt == 0 || userInputInt == 1) {
+        return 1;
+    } else {
+        return userInputInt * factorial(userInputInt - 1);
+    }
 }
 
 int main() {
-   std::string userInput;
-   int userInputInt;
+    std::string userInput;
+    int userInputInt;
 
-   // Input prompt
-   std::cout << "Enter a positive integer: ";
+    // Input prompt
+    std::cout << "Enter a positive integer: ";
 
-   // Read input as a string
-   std::getline(std::cin, userInput);
+    // Read input as a string
+    std::getline(std::cin, userInput);
 
-   // Try to convert input to an integer
-   std::stringstream inputStream(userInput);
-   if (!(inputStream >> userInputInt)) {
-       std::cout << "That was not a number." << std::endl;
-   } else {
-       // Call factorial function
-       int factorialAnswer = factorial(userInputInt);
-       if (factorialAnswer === -1) {
-          std::cout << "That was not a positive integer." << std::endl;
-       } else {
-       std::cout << "The factorial of " << userInputInt <<
-       " is " << factorialAnswer << std::endl;
+    // Try to convert input to an integer
+    std::stringstream inputStream(userInput);
+    if (!(inputStream >> userInputInt)) {
+        std::cout << "That was not a number." << std::endl;
+    } else {
+        // Call factorial function
+        int factorialAnswer = factorial(userInputInt);
+        if (factorialAnswer == -1) {
+            std::cout << "That was not a positive integer." << std::endl;
+        } else {
+            std::cout << "The factorial of " << userInputInt <<
+            " is " << factorialAnswer << std::endl;
        }
-   }
+    }
 
-   std::cout << "\nDone." << std::endl;
-   return 0;
+    std::cout << "\nDone." << std::endl;
+    return 0;
 }
